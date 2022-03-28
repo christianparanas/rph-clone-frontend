0ddfd95da9788f6626b297218b056c1f3814492a
kate <christiannparanas@gmail.com>
2022-03-28 19:43:09 +0800

    feat: marquee section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,56 @@
 import Head from "next/head";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
+// import styles
+import styles from "styles/home.module.scss";
+
+// components
 import LandingHeader from "components/landingheader/landingheader";
 import LandingHero from "components/landinghero/landinghero";
 import Page from "components/page/page";
+
+const jobs = [
+  [
+    "HTML",
+    "CSS",
+    "Programming",
+    "Mobile App",
+    "Software Architecture",
+    "UI / UX Design",
+    "C++ Programming",
+    "Computer Security",
+    "eCommerce",
+    "Debugging",
+    "GitHub",
+  ],
+  [
+    "HTML",
+    "CSS",
+    "Programming",
+    "Mobile App",
+    "Software Architecture",
+    "UI / UX Design",
+    "C++ Programming",
+    "Computer Security",
+    "eCommerce",
+    "Debugging",
+    "GitHub",
+  ],
+  [
+    "HTML",
+    "CSS",
+    "Programming",
+    "Mobile App",
+    "Software Architecture",
+    "UI / UX Design",
+    "C++ Programming",
+    "Computer Security",
+    "eCommerce",
+    "Debugging",
+    "GitHub",
+  ],
+];
 
 export default function Home() {
   return (
@@ -11,8 +58,27 @@ export default function Home() {
       <LandingHeader />
       <LandingHero />
 
-      <div className="">
-        
+      <div className={styles.marquee_container}>
+        <div className={styles.marquee_wrapper}>
+          <h1>You belong here</h1>
+          <div className={styles.content}>
+            {jobs.map((job, key) => {
+              return (
+                <Marquee
+                  speed={60}
+                  gradient={false}
+                  direction={key == 1 ? "left" : "right"}
+                  pauseOnHover={true}
+                  className={styles.mq}
+                >
+                  {job.map((skill, idx) => {
+                    return <h2>{skill}</h2>;
+                  })}
+                </Marquee>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </Page>
   );
