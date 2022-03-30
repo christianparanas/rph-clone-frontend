716c47b36073675361482db60ea9039d574b12ad
kate <christiannparanas@gmail.com>
2022-03-30 16:09:11 +0800

    feat: login

---
+++ components/icons/index.js
@@ -0,0 +1,4 @@
+export { default as Google } from "./google";
+export { default as Facebook } from "./facebook";
+export { default as LinkedIn } from "./linkedIn";
+export { default as Email } from "./email";
--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,48 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link"
 import { Fragment } from "react";
 
 // import styles
-import styles from "styles/home.module.scss";
+import styles from "styles/login.module.scss";
 
 // components
-import Page from 'components/page/page';
+import Page from "components/page/page";
+import { Google, Facebook, LinkedIn, Email } from "components/icons";
 
 export default function Login() {
   return (
     <Page>
-      Login
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.box}>
+            <h1>Welcome back</h1>
+
+            <div className={styles.providers}>
+              <div className={styles.provider}>
+                <Facebook />
+                Login with Facebook
+              </div>
+              <div className={styles.provider}>
+                <Google />
+                Login with Google
+              </div>
+              <div className={styles.provider}>
+                <LinkedIn />
+                Login with LinkedIn
+              </div>
+              <div className={styles.provider}>
+                <Email />
+                Login with Email
+              </div>
+            </div>
+
+            <div className={styles.signup_wrapper}>
+              No account? <Link href="/signup"><span>Sign Up</span></Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </Page>
   );
 }
